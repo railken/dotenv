6cc34676e2197216246cae8aacb65b128a8cbef0
railken <railken@protonmail.ch>
2018-12-26 20:38:48 +0100

    refactor

--- src/Storage.php
+++ src/Storage.php
@@ -28,54 +28,11 @@ class Storage
      * Store the variable in the .env file.
      *
      * @param string $key
-     */
-    public function prepare(string $key)
-    {
-        $content = file_get_contents($this->filePath);
-        $oldValue = getenv($key);
-
-        $oldValue = str_replace('$', '\$', $oldValue);
-
-        $variable = new Variable();
-
-        if (preg_match(sprintf('/%s=%s/', $key, $oldValue), $content, $matches)) {
-            $variable->setOriginal($matches[0]);
-        }
-
-        if (preg_match(sprintf('/%s="%s"/', $key, $oldValue), $content, $matches)) {
-            $variable->setOriginal($matches[0]);
-        }
-
-        if (!$variable->getOriginal()) {
-            throw new InvalidKeyValuePairException(sprintf('%s=%s', $key, $oldValue));
-        }
-
-        return $variable;
-    }
-
-    /**
-     * Replace string in .env file.
-     *
-     * @param Variable $variable
-     */
-    public function persistVariable(Variable $variable)
-    {
-        if ($variable->getOriginal()) {
-            file_put_contents($this->filePath, str_replace($variable->getOriginal(), $variable->toFile(), file_get_contents($this->filePath)));
-        } else {
-            file_put_contents($this->filePath, file_get_contents($this->filePath).PHP_EOL.$variable->toFile());
-        }
-    }
-
-    /**
-     * Store the variable in the .env file.
-     *
-     * @param string $key
      * @param mixed  $value
      */
     public function store(string $key, $value = null)
     {
-        $variable = $this->prepare($key);
+        $variable = $this->extract($key);
 
         $variable->setKeyFromRaw($key);
         $variable->setValueFromRaw($value);
@@ -107,11 +64,54 @@ class Storage
      */
     public function remove(string $key)
     {
-        $variable = $this->prepare($key);
+        $variable = $this->extract($key);
         $variable->setKeyFromRaw($key);
         $variable->setValue(false);
         $this->persistVariable($variable);
 
         return $variable;
+    }
+
+    /**
+     * Store the variable in the .env file.
+     *
+     * @param string $key
+     */
+    protected function extract(string $key)
+    {
+        $content = file_get_contents($this->filePath);
+        $oldValue = getenv($key);
+
+        $oldValue = str_replace('$', '\$', $oldValue);
+
+        $variable = new Variable();
+
+        if (preg_match(sprintf('/%s=%s/', $key, $oldValue), $content, $matches)) {
+            $variable->setOriginal($matches[0]);
+        }
+
+        if (preg_match(sprintf('/%s="%s"/', $key, $oldValue), $content, $matches)) {
+            $variable->setOriginal($matches[0]);
+        }
+
+        if (!$variable->getOriginal()) {
+            throw new InvalidKeyValuePairException(sprintf('%s=%s', $key, $oldValue));
+        }
+
+        return $variable;
+    }
+
+    /**
+     * Replace string in .env file.
+     *
+     * @param Variable $variable
+     */
+    protected function persistVariable(Variable $variable)
+    {
+        if ($variable->getOriginal()) {
+            file_put_contents($this->filePath, str_replace($variable->getOriginal(), $variable->toFile(), file_get_contents($this->filePath)));
+        } else {
+            file_put_contents($this->filePath, file_get_contents($this->filePath).PHP_EOL.$variable->toFile());
+        }
     }
 }
